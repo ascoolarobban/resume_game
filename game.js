ddec4e655b8eabad01d9da617379ad4f9d72ed6f
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2022-06-30 16:07:49 +0200

    sound button on mobile fix

--- game.js
+++ game.js
@@ -29,11 +29,11 @@ fireAudio.loop = true;
 
 
 //CURSOR STUFF
-var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function(e) {
-    cursor.style.left = e.clientX + "px",
-        cursor.style.top = e.clientY + "px";
-});
+// var cursor = document.getElementById("cursor");
+// document.body.addEventListener("mousemove", function(e) {
+//     cursor.style.left = e.clientX + "px",
+//         cursor.style.top = e.clientY + "px";
+// });
 
 
 
