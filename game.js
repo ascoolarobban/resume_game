52a03dfd21164584f1566713833790e3beb62485
Robin Ellingsen <70570646+ascoolarobban@users.noreply.github.com>
2022-06-30 17:42:50 +0200

    fix

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 //SOUND STUFF
 fireAudio = new Audio('fire.wav');
